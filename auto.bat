f047942b16c6475bb37183626d4e927932eb000a
Adson Breno <101665691+AdsonBreno@users.noreply.github.com>
2023-10-20 13:06:48 -0300

    Delete auto.bat

--- auto.bat
+++
@@ -1,7 +0,0 @@
-cd C:\Users\adson\OneDrive\Documentos\Arduino
-git init
-git add .
-git status
-git commit -m "First commit"
-git remote add origin https://github.com/AdsonBreno/Arduino.git
-git push -u origin master

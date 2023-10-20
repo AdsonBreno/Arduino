7e68fbfa7d02fe5888abcc6014a9ec575ada2b92
Adson Breno <101665691+AdsonBreno@users.noreply.github.com>
2023-10-20 15:32:39 -0300

    Add files via upload

--- ESP32/ESP32.ino
+++ ESP32/ESP32.ino
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <PubSubClient.h>
 
 //Digital Inputs
-#define dataPin 23
 #define sPin 22 //Sensor
 #define aPin 4 //Analogic
 #define magPin 21 //Magnetic
@@ -11,85 +11,121 @@
 #define pulsePin 15
 #define constPin 16
 
+//ID MQTT
+#define pubMag "MAGS"
+#define subLock "DOOR"
+#define TOPIC_PUBLISH "HALLWAY"
+
+WiFiClient wifiClient;
+PubSubClient MQTT(wifiClient);
+PubSubClient MQTT2(wifiClient);
+
 //Consts
-const char* ssid = "Sensor_Hall";
-const char* pass = "Hall1234";
+const char* ssid = "LAT-VISITAS";
+const char* pass = "@lat@public";
 const char* broker = "public.mqtthq.com";
 
+int port = 1883;
 int pState = HIGH;
 int state, analogic, magnetic;
-int port = 1883;
 
-void setupWiFi(){
-  delay(100);
-  Serial.print("\nConnecting to");
+void setup() {
+  Serial.begin(9600);
+  pinMode(pulsePin, OUTPUT);
+  pinMode(magPin, INPUT_PULLUP);
+
+  digitalWrite(constPin, HIGH);
+  connectWiFi();
+  MQTT.setServer(broker, port);
+  MQTT2.setServer(broker, port);
+  MQTT2.setCallback(receivePacket);
+}
+
+void connectWiFi(){
+  Serial.println("Connecting to");
   Serial.println(ssid);
   Serial.print("...");
 
   WiFi.begin(ssid, pass);
 
-  while(WiFi.status() != WL_CONNECTED){
-    delay(100);
+  if (WiFi.status() == WL_CONNECTED){
     Serial.print("...");
+    return;
   }
 
-  Serial.print("\nConnected to");
-  Serial.println(ssid);
+  Serial.println("Connecting to ");
+  Serial.print(ssid);
   Serial.print(".");
-}
 
-void setup() {
-  pinMode(pulsePin, OUTPUT);
-  pinMode(constPin, OUTPUT);
-  pinMode(magPin, INPUT_PULLUP);
-  pinMode(sPin, INPUT_PULLUP);
-  pinMode(dataPin, INPUT_PULLUP);
-  digitalWrite(constPin, HIGH);
-  setupWiFi();
+  while(WiFi.status() != WL_CONNECTED){
+  delay(100);
+  Serial.print("...");
+  }
+  Serial.println("Connected to ");
+  Serial.print(ssid);
+  Serial.print(".");
+  Serial.println(WiFi.localIP());
 }
-void pulse500() {
-  state = digitalRead(dataPin);
+void connectMQTT(){
+  while (!MQTT.connected()){
+    Serial.print("Connecting to MQTT Broker ");
+    Serial.print(broker);
+    Serial.print(".");
+    if (MQTT.connect(pubMag)){
+      Serial.println("Connected to Broker.");
+    }
+    if (MQTT2.connect(subLock)){
+      Serial.println("Connected to Broker.");
+      MQTT2.subscribe(TOPIC_PUBLISH);
+    }
+    else if(!MQTT.connected()){
+      Serial.println("Could not connect to broker. New attempt in 10 seconds.");
+      delay(10000);
+    }
+  }
+}
+void keepConnections(){
+  if(!MQTT.connected()){
+      connectMQTT();
+  }
+  connectWiFi();
+}
+void receivePacket(char* topic, byte* payload, unsigned int length){
+  String msg;
 
-    if (pState == LOW && state == HIGH) {
+  for(int i = 0; i < length; i++){
+    char c = (char)payload[i];
+    msg += c;
+  }
+  if (msg == "0"){
     digitalWrite(pulsePin, HIGH);
     delay(500);
-    if (state == HIGH)
-      digitalWrite(pulsePin, LOW);
-      delay(3000);
   }
-
-  pState = state;
+  if (msg == "1"){
+    digitalWrite(pulsePin, LOW);
+    delay(3000);
+  }
 }
+
 void magSensor(){
   magnetic = digitalRead(magPin);
   
   if (magnetic == HIGH){
-    Serial.begin(9600);
     digitalWrite(pulsePin, HIGH);
+    MQTT.publish(TOPIC_PUBLISH, "1");
     Serial.println("Porta Aberta");
     delay(3000);
+
   } else if (magnetic == LOW) {
     digitalWrite(pulsePin, LOW);
+    MQTT.publish(TOPIC_PUBLISH, "0");
     Serial.println("Porta Fechada");
     delay(3000);
   }
 }
-void sensorPIR() {
-  bool sPIR = digitalRead(sPin);
 
-  if(sPIR) {
-    Serial.begin(9600);
-    digitalWrite(pulsePin, HIGH);
-    Serial.println("ACIONADO");
-    delay(1000);
-  } else {
-    digitalWrite(pulsePin, LOW);
-    Serial.println("---");
-  }
-}
 void loop() {
-
-//  analogic = analogRead(aPin); //Retorna 0~4096 0,0080566406
-//  digitalWrite(constPin, HIGH); //3,3V Constant
+  keepConnections();
+  MQTT.loop();
   magSensor();
 }
